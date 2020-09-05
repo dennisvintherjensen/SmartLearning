bdde711a1b8ee87a3449b25cce41c31dd886daec
Dennis Jensen <dennis.jensen@idnet.dk>
2020-09-06 01:26:01 +0200

    Task 1-3

---
+++ ExtendedProgrammingWeek1Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExtendedProgrammingWeek1Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const decimal centimetersInAnInch = 2.54M;
+
+            decimal amountOfInches = 3.23M;
+
+            decimal inchesAsCentimeters = amountOfInches * centimetersInAnInch;
+
+            Console.WriteLine($"{amountOfInches} inches is {inchesAsCentimeters} centimeters");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ExtendedProgrammingWeek1Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ExtendedProgrammingWeek1Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const decimal centimetersInAnInch = 2.54M;
+
+            bool? parsed = false;
+
+            do
+            {
+                Console.WriteLine("Enter amount of inches that should be converted to centimeters.");
+
+                string amountOfInchesAsString = Console.ReadLine();
+
+                if (decimal.TryParse(amountOfInchesAsString, out decimal amountOfInches))
+                {
+                    parsed = true;
+
+                    decimal inchesAsCentimeters = amountOfInches * centimetersInAnInch;
+
+                    Console.WriteLine($"{amountOfInches} inches is {inchesAsCentimeters} centimeters");
+                }
+                else {
+                    Console.WriteLine("Incorrect format, try again...");
+                }
+            }
+            while (parsed == false);
+
+            Console.ReadLine();
+        }
+    }
+}
