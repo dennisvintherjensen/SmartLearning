66541f64b30b7f4dadd92e9a2cbd6e32222597e1
Dennis Jensen <dennis.jensen@idnet.dk>
2020-09-07 01:21:20 +0200

    Added solutions

---
+++ ExtendedProgrammingWeek1Task4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ExtendedProgrammingWeek1Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a positive whole number to FizzBuzz - and Jazz");
+
+            int n = PromptAmount();
+
+            for (int i = 1; i <= n; i++)
+            {
+                string output = String.Empty;
+
+                if (i % 3 == 0)
+                {
+                    output += "Fizz";
+                }
+
+                if (i % 5 == 0)
+                {
+                    output += "Buzz";
+                }
+
+                if (i % 7 == 0)
+                {
+                    output += "Jazz";
+                }
+
+                Console.WriteLine(string.IsNullOrEmpty(output) ? i.ToString() : output);
+            }
+        }
+
+        static int PromptAmount()
+        {
+            if (int.TryParse(Console.ReadLine(), out int amount))
+            {
+                return amount;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect format, try again...");
+
+                return PromptAmount();
+            }
+        }
+    }
+}
---
+++ ExtendedProgrammingWeek1Task5/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ExtendedProgrammingWeek1Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter text for palindrome test.");
+
+            string input = PromptInput();
+
+            string cleanedInput = CleanAndLowercaseInput(input);
+
+            if (IsPalindrome(cleanedInput))
+            {
+                Console.WriteLine($@"""{input}"" is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"{input} is NOT a palindrome.");
+            }
+
+            Console.WriteLine("Press any key to exit");
+
+            Console.ReadLine();
+        }
+
+        static bool IsPalindrome(string input)
+        {
+            int midSentence = (int)decimal.Round(input.Length / 2);
+
+            for (int i = 0; i <= input.Length; i++)
+            {
+                if (i > midSentence)
+                {
+                    break;
+                }
+
+                char left = input[i];
+
+                char right = input[input.Length - 1 - i];
+
+                if (left != right)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        static string CleanAndLowercaseInput(string input)
+        {
+            return Regex.Replace(input, "[^a-z0-9]", "", RegexOptions.IgnoreCase).ToLower();
+        } 
+
+        static string PromptInput()
+        {
+            string input = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+            else
+            {
+                Console.WriteLine("You need to input some text. try again...");
+
+                return PromptInput();
+            }
+        }
+    }
+}
